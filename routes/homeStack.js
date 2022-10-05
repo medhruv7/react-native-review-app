37fd1c4dc19f3845f4c64a00b5d991b61fd259be
= <=>
2022-10-06 03:33:08 +0530

    header options

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,17 +5,29 @@ import Review from "../screens/review";
 import About from "../screens/about";
 
 const screens = {
-    Home: {
-        screen: Home,
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: "Test12",
+      headerStyle: { height: 100 },
     },
-    Review: {
-        screen: Review,
+  },
+  Review: {
+    screen: Review,
+    navigationOptions: {
+      title: "Reviews Page",
     },
-    About: {
-        screen: About,
-    }
+  },
+  About: {
+    screen: About,
+  },
 };
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerStyle: { backgroundColor: "#eee" },
+    headerTintColor: '#444',
+  },
+});
 
 export default createAppContainer(HomeStack);
